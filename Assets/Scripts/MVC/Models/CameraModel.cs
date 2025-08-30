1200c5e768587ba589d013c4ecb7ece335b1f937
Acrobaticbag3 <kevintom214@gmail.com>
2025-08-31 00:45:39 +0200

    Added CameraModel
    Initail setup of camera model script

--- Assets/Scripts/MVC/Models/CameraModel.cs
+++ Assets/Scripts/MVC/Models/CameraModel.cs
@@ -3,5 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraModel : MonoBehaviour {
-    
+    // Internal
+    [SerializeField] private Vector2 position = Vector2.zero;
+    [SerializeField] private float rotation = 0f;
+    [SerializeField] private float zoom = 2f;
+
+    // Tweakable
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private float maxZoom = 20f;
+    [SerializeField] private float minZoom = 2f;
+
+    // Controlled access
+    public Vector2 Position {
+        get => position;
+        set => position = value;
+    }
+
+    public float Rotation {
+        get => rotation;
+        set => rotation = value;
+    }
+
+    public float Zoom {
+        get => zoom;
+        set => zoom = Mathf.Clamp(value, minZoom, maxZoom);
+    }
+
+    // Read only --> Potential later use, maybe remove??
+    public float MoveSpeed => moveSpeed;
+    public float ZoomSpeed => zoomSpeed;
+    public float MaxZoom => maxZoom;
+    public float MinZoom => minZoom;
 }
